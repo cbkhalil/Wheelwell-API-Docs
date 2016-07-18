825aff594f23ac99e532c33e3ebb4cc4571f5197
Brynn Khalil <cbkhalil@Brynns-MacBook.local>
2016-07-17 20:58:22 -0700

    Added gulp-s3-upload functionality

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,46 @@
-var gulp 			= require('gulp')
-var inline 			= require('gulp-inline')
-var jade 			= require('gulp-jade')
-var minifyCSS 		= require('gulp-minify-css')
-var uglify 			= require('gulp-uglify')
+
+const config = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
+
+const gulp 			= require('gulp')
+const inline 		= require('gulp-inline')
+const pug 			= require('gulp-pug')
+const minifyCSS 	= require('gulp-minify-css')
+const uglify 		= require('gulp-uglify')
+const s3 			= require('gulp-s3-upload')(config)
 
 
 
-gulp.task('default', function() {
-	gulp.src(['./client/*.jade'])
-	.pipe(jade({
-		locals: require('./api-docs.json'),
-		doctype: 'html'
-	}))
-	.pipe(inline({
-		js: uglify,
-		css: minifyCSS
-	}))
-	.pipe(gulp.dest('./dist/'))
+gulp.task('render', function() {
+	return 	gulp.src(['./client/*.pug'])
+			.pipe(pug({
+				locals: require('./api-docs.json'),
+				doctype: 'html'
+			}))
+			.pipe(inline({
+				js: uglify,
+				css: minifyCSS
+			}))
+			.pipe(gulp.dest('./dist/'))
 })
+
+
+
+gulp.task("upload", ['render'], function() {
+    return 	gulp.src("./dist/index.html")
+		    .pipe(s3({
+		        Bucket: process.env.AWS_S3_BUCKET_NAME,
+		        ACL:    'public-read'
+		    }, 
+		    {
+		        maxRetries: 5
+		    }))
+})
+
+
+
+gulp.task('default', ['upload'])
+
 
